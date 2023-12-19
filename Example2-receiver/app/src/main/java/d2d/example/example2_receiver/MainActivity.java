656aa7dc4098c12475c6b3323a2fb2c4f58a6bfb
Miguel Verdaguer Velázquez <verdx@riseup.net>
2023-12-19 19:16:47 +0100

    Add new Example2-receiver

---
+++ Example2-receiver/app/src/main/java/d2d/example/example2_receiver/MainActivity.java
@@ -0,0 +1,39 @@
+package d2d.example.example2_receiver;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+
+/**
+ * A straightforward example of how to stream AMR and H.263 to some public IP using libstreaming.
+ * Note that this example may not be using the latest version of libstreaming !
+ */
+public class MainActivity extends Activity implements OnClickListener {
+
+    private final static String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+    }
+}
